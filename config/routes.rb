4a545ce317e6e9033bfc25ab51c10c079e6adf91
ackers93 <andrew.ackers@gmail.com>
2025-10-08 22:35:55 -0700

    routes and views for google places requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :leads
+  
+  # Bulk import routes
+  get 'bulk_imports/new', to: 'bulk_imports#new', as: :new_bulk_import
+  post 'bulk_imports/search', to: 'bulk_imports#search', as: :search_bulk_imports
+  post 'bulk_imports/import', to: 'bulk_imports#import', as: :import_bulk_imports
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
